885970b074a0489e7f36535880b847aa7dcb961a
Itzel Doroteo Mtz <20211311@UTHH.EDU.MX>
2023-10-19 09:49:08 -0600

    Update-Files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
-//Practicamente este es el que se modifica 
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <FrmCifrado />
+  </div>
 </template>
-  
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FrmCifrado from './components/FrmCifrado.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    FrmCifrado
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Estilos de la aplicación principal */
 </style>
